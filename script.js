141edeb124e50aefe9e6955a13caa90bedac8388
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-18 13:14:16 +0330

    js 1

---
+++ script.js
@@ -0,0 +1,5 @@
+let pass1=document.querySelector('#pass_1');
+let pass2=document.querySelector('#pass_2');
+let button=document.querySelector('button');
+let showBtn=document.querySelector('.show');
+let errorText=document.querySelector('.error-text');
