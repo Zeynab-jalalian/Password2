eb0e7b5a24dc0b1b445478396e4cb247c30228f1
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-18 13:32:34 +0330

    js f active

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let pass2=document.querySelector('#pass_2');
 let button=document.querySelector('button');
 let showBtn=document.querySelector('.show');
 let errorText=document.querySelector('.error-text');
+
+function active(){
+    if(pass1.value.length>=6){
+        button.removeAttribute('disabled','');
+        pass2.removeAttribute('disabled','');
+        button.classList.add('active');
+    }else{
+        button.setAttribute('disabled','');
+        pass2.setAttribute('disabled','');
+        button.classList.remove('active');
+    }
+}
